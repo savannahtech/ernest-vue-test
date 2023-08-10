42588b350a43aee4e58f39fc315fd091e2b6f0cf
Ernest Brayce <gamiskah@gmail.com>
2023-08-10 15:58:06 +0000

    feat: add tag card component

---
+++ src/components/tags-card/TagsCard.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+<div class='tags-card-container'>
+
+</div>
+</template>
+
+<style scoped>
+.tags-card-container {
+  padding: 1.5rem;
+  width: 13.9375rem;
+  min-height: 11.125rem;
+  flex-shrink: 0;
+  border-radius: 10px;
+  background: #fff;
+}
+</style>
